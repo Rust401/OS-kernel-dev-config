bab5e85c76f1532d8886001d0393c2a9c91f9fea
Ruty Dude <zhaodonghu94@gmail.com>
2021-12-08 23:29:51 +0800

    Description:add a script for cscope database generating
    Team:OTHERS
    Feauture/Bugfix:Feature
    Binary:No

---
+++ dude_script/csgen.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+TARGET=$1
+VALID=0
+
+#add new linux-kernel dir here
+if [ $TARGET = "qcom414" ]; then
+LNX_PATH="/home/ruty/ext/qcom414/msm-4.14"
+VALID=1
+fi
+
+if [ $VALID = "1" ]; then
+
+#find paths for files to be linked
+echo start to generate cscope.files in $LNX_PATH
+find $LNX_PATH                                                                     \
+	-path "$LNX_PATH/arch/*" ! -path "$LNX_PATH/arch/arm64*" -prune -o         \
+	-path "$LNX_PATH/include/asm-*" -prune -o                                  \
+	-path "$LNX_PATH/tmp*" -prune -o                                           \
+	-path "$LNX_PATH/Documentation*" -prune -o                                 \
+	-path "$LNX_PATH/scripts*" -prune -o                                       \
+	-path "$LNX_PATH/drivers*" -prune -o                                       \
+	-name "*.[chxsS]" -print > $LNX_PATH/cscope.files
+echo cscope.files generated !
+
+#generate cscope database
+echo change dir
+cd $LNX_PATH
+echo begin to generate cscope database
+cscope -b -k -q
+echo database generated in cscope.out
+cd -
+echo change dir back
+
+fi
